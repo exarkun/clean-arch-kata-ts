b3575dc7874f55743952196c6067d599e3246f87
Jean-Paul Calderone <jeanpaul.calderone@converge.io>
2025-02-26 16:55:29 -0500

    misc: setup development environment

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Clean Architecture Code Kata";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            alejandra
+            nil
+            nodePackages.typescript-language-server
+            nodejs_20
+            nodePackages.pnpm
+          ];
+
+          shellHook = ''
+            export PATH="$PWD/node_modules/.bin:$PATH"
+          '';
+        };
+      };
+    };
+}
